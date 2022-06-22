feea12f243a2dc261f0646a80cdacd6ef681f35f
David Benoit <d.benoit@hull.ac.uk>
2022-06-22 23:08:06 +0100

    setup fix (from James)

--- setup.py
+++ setup.py
@@ -18,6 +18,5 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    setup_requires=["setuptools_scm~=6.0"],
     install_requires=["z-quantum-core"],
 )
